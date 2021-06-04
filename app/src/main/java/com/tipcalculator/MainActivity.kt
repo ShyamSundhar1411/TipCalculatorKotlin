8a2ca2c82f6646e58845a1c84849b023eb0a5bfb
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2021-06-04 18:09:00 +0530

    Wrapping Up

--- app/src/main/java/com/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/tipcalculator/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.tipcalculator
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        infoTextView.visibility = View.INVISIBLE
+        Calculate.setOnClickListener {
+            val bill = billEditText.text.toString().toDouble()
+            val tipper = tipEditText.text.toString().toDouble()
+            val tip = bill * (tipper/100)
+            val total = bill+tip
+            infoTextView.visibility = View.VISIBLE
+            val result = "Tip : ${formatdeci(tip)} Total : ${formatdeci(total)}"
+            infoTextView.text = result
+        }
+    }
+    fun formatdeci(n1 : Double): String {
+        return "\$"+String.format("%.2f",n1)
     }
 }
